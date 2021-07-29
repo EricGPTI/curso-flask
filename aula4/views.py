67b79ac055056c4f4644bb1582e823b04a38a1a5
EricGPTI <ericgpti@gmail.com>
2021-07-28 23:20:05 -0300

    Finalizado explicação sobre arquitetura e factories 🏭

--- aula4/views.py
+++ aula4/views.py
@@ -1,11 +1,13 @@
-from app import app
+"""Extensão do Flask - Uma coisa que é chamada."""
 
 
-@app.route("/")
-def index():
-    return "<h1>Hello World</h1>"
+def init_app(app):
+    """Factory de inicialização de extensões"""
 
+    @app.route("/")
+    def index():
+        return "<h1>Hello World</h1>"
 
-@app.route("/contato")
-def contato():
-    return "<form><input type='text'></input></form>"
+    @app.route("/contato")
+    def contato():
+        return "<form><input type='text'></input></form>"
